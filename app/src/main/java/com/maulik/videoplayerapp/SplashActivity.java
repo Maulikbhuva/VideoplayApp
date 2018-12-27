c802d32e6099d93a4918c3f0474768ae949dc1cd
Maulikbhuva <maulikpb007@gmail.com>
2018-12-27 17:02:38 +0530

    started

---
+++ app/src/main/java/com/maulik/videoplayerapp/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.maulik.videoplayerapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private Runnable runnable;
+    private static final String TAG = "Welcome";
+    private Handler mWaitHandler = new Handler();
+    private Boolean value=false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, VideoViewActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+
+        }, 2000);  // Give a 5 seconds delay.
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //Remove all the callbacks otherwise navigation will execute even after activity is killed or closed.
+        mWaitHandler.removeCallbacksAndMessages(null);
+    }
+
+}
---
+++ app/src/main/java/com/maulik/videoplayerapp/VideoPlayerApp.java
@@ -0,0 +1,13 @@
+//package com.maulik.videoplayerapp;
+//
+//import android.app.Application;
+//import android.content.Context;
+//import android.support.multidex.MultiDex;
+//
+//public class VideoPlayerApp extends Application {
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//        super.attachBaseContext(base);
+//        MultiDex.install(this);
+//    }
+//}
